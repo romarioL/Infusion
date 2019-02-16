825dd62f5b1523f38c00acceaf7cb396e9c87dea
romarioL <45340806+romarioL@users.noreply.github.com>
2019-02-15 22:42:43 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+(function(){
+   var $body = document.querySelector('body')
+   $body.classList.remove('no-js')
+   $body.classList.add('js')
+
+
+
+   var menu = new Menu({
+      container: '.header-nav',
+      toggleBtn: '.header-btn-menu', 
+      widthEnabled: 1024
+   })
+   
+   var carouselImgs = new Carousel({
+       container: '.laptop-slider .slideshow',
+       itens: 'figure',
+       btnPrev: '.prev',
+       btnNext: '.next'
+   })
+   
+   var carouselQuotes = new Carousel({
+       container: '.quote-slideshow',
+       itens: 'figure',
+       btnPrev: '.prev',
+       btnNext: '.next'
+   })
+   
+    
+})()
